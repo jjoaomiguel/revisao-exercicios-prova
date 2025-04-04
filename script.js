4adc420a242fbb00bb0c35bcbc29b4118696600c
João <joao_m_medeiros@estudante.sesisenai.org.br>
2025-04-04 09:00:58 -0300

    add - exercicio 1 feito

--- script.js
+++ script.js
@@ -4,20 +4,16 @@ A cada novo número digitado, o valor deve ser acumulado.
 Ao final, exiba o resultado total da soma.
 
 */
-let n = parseFloat(prompt('Digite um número: '))
-let n1 = parseFloat(prompt('Digite mais um número: '))
-let pergunta = prompt ('Você deseja digitar mais um número?')
+let n = parseFloat (prompt('Quantos números você deseja somar?'));
+let soma = 0;
 
-for (i)
+    for (let i = 0; i < n; i++) {
+        let pergunta = parseFloat (prompt ('Digite um número: '));
+        soma = pergunta + soma;
+    }
+console.log('A soma é: ' + soma)
 
-if (pergunta = 'sim') {
-    let n2 = parseFloat(prompt ('Digite mais um número: '))
-    let soma = n + n1 + n2
-    console.log('A soma é: ' + soma)
-}else if (pergunta = 'não'){
-    let soma = n + n1
-    console.log('A soma é: ' + soma)
-}
+
 
 
 /* Exercício 2
@@ -25,4 +21,3 @@ Simule uma contagem regressiva para o lançamento de um foguete,
 partindo de 10 até 0, e ao final exiba a mensagem "Lançamento!".
 
 */
-
