94cc58318cf994668bed88472fa73170059ee640
João <joao_m_medeiros@estudante.sesisenai.org.br>
2025-04-04 08:46:10 -0300

    add - fazendo a 1

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Exercício 1
+Crie um algoritmo que solicite ao usuário quantos números ele quiser somar. 
+A cada novo número digitado, o valor deve ser acumulado. 
+Ao final, exiba o resultado total da soma.
+
+*/
+let n = parseFloat(prompt('Digite um número: '))
+let n1 = parseFloat(prompt('Digite mais um número: '))
+let pergunta = prompt ('Você deseja digitar mais um número?')
+
+for (i)
+
+if (pergunta = 'sim') {
+    let n2 = parseFloat(prompt ('Digite mais um número: '))
+    let soma = n + n1 + n2
+    console.log('A soma é: ' + soma)
+}else if (pergunta = 'não'){
+    let soma = n + n1
+    console.log('A soma é: ' + soma)
+}
+
+
+/* Exercício 2
+Simule uma contagem regressiva para o lançamento de um foguete, 
+partindo de 10 até 0, e ao final exiba a mensagem "Lançamento!".
+
+*/
+
